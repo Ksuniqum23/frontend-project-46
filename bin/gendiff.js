00f333dcfbcd5e421b3705372754ea88f67ebbbc
Kseniya Freeflyer <kalyakina.ks.a@gmail.com>
2024-08-09 13:22:01 +0300

    add gendiff

---
+++ bin/gendiff.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+const program = new Command();
+
+program
+    .name('gendiff')
+    .description('Compares two configuration files and shows a difference.')
+    .version('1.0.0')
+    .option('-h, --help', 'output usage information');
+
+program.parse(process.argv);
+
+if (program.opts().help) {
+    program.help();
+}
+
+//https://www.perplexity.ai/search/dlia-chego-nuzhen-etot-proekt-seONF3MiRD6C1hGWvAzwtg
