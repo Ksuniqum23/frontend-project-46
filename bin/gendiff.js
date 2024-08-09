ef79966b914600d1c38811dcc9c10c150e817a0f
Kseniya Freeflyer <kalyakina.ks.a@gmail.com>
2024-08-09 16:00:19 +0300

    add option format <type>

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,12 +7,16 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-h, --help', 'output usage information');
-
+  .option('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format');
 program.parse(process.argv);
 
 if (program.opts().help) {
   program.help();
 }
+
+// program.action((path1, path2) => {
+//   console.log(sum(path1, path2, program.opts().format));
+// })
 
 // https://www.perplexity.ai/search/dlia-chego-nuzhen-etot-proekt-seONF3MiRD6C1hGWvAzwtg
