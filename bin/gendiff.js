c3fce93ed918dc7162b91cb43299531a031a0c1b
Kseniya Freeflyer <kalyakina.ks.a@gmail.com>
2024-08-26 17:44:10 +0300

    3-th step working!

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,33 +1,32 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import genDiff  from '../index.js';
+import genDiff from '../index.js';
 
 const program = new Command();
 
 const justDoIt = (file1, file2) => {
-    genDiff(file1, file2);
+  genDiff(file1, file2);
 };
 
 program
-    // .command('1 <file1> <file2>')
-    // .name('gendiff')
-    .arguments('<file1> <file2>')
-    .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0')
-    .option('-f, --format <type>', 'output format')
-    .action(justDoIt)
-    // .parse(process.argv);
-    // .action((path1, path2) => {
-    //     try {
-    //         genDiff(path1, path2);
-    //     } catch (err) {
-    //         console.error(`Ошибка: ${err.message}`);
-    //     }
-    // });
+// .command('1 <file1> <file2>')
+// .name('gendiff')
+  .arguments('<file1> <file2>')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .option('-f, --format <type>', 'output format')
+  .action(justDoIt);
+// .parse(process.argv);
+// .action((path1, path2) => {
+//     try {
+//         genDiff(path1, path2);
+//     } catch (err) {
+//         console.error(`Ошибка: ${err.message}`);
+//     }
+// });
 
 // Парсинг аргументов командной строки
 program.parse(process.argv);
-
 
 // https://youtu.be/_Oe4B6OKPvY?si=OYQ3DR3aU-kDF4Af&t=3142
 // https://www.perplexity.ai/search/dlia-chego-nuzhen-etot-proekt-seONF3MiRD6C1hGWvAzwtg
