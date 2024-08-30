2ff19a806126120eafb6cd562f1e7d55f51d7a93
Kseniya Freeflyer <kalyakina.ks.a@gmail.com>
2024-08-30 15:32:09 +0300

    4-th step wip

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,10 +1,38 @@
 import path from 'path';
 import * as fs from 'fs';
+import _ from 'lodash';
 import parser from './parser.js';
 
 const getFullPath = (filePath) => path.resolve(process.cwd(), filePath);
 const extractFormat = (filePath) => path.extname(filePath).slice(1);
 const getData = (filePath) => parser(fs.readFileSync(filePath, 'utf-8'), extractFormat(filePath));
+
+const differents = (obj1, obj2) => {
+  const obj1keys = Object.keys(obj1);
+  const obj2keys = Object.keys(obj2);
+  const resultKeys = _.union(obj1keys, obj2keys).sort();
+  const result = {};
+  for (let i = 0; i < resultKeys.length; i += 1) {
+    const key = resultKeys[i];
+    if (Object.hasOwn(obj1, key) === true && Object.hasOwn(obj2, key) === true) {
+      if (obj1[key] === obj2[key]) {
+        result[`  ${key}`] = obj1[key];
+      } else {
+        result[`- ${key}`] = obj1[key];
+        result[`+ ${key}`] = obj2[key];
+      }
+    }
+
+    if (Object.hasOwn(obj1, key) === true && Object.hasOwn(obj2, key) === false) {
+      result[`- ${key}`] = obj1[key];
+    }
+
+    if (Object.hasOwn(obj1, key) === false && Object.hasOwn(obj2, key) === true) {
+      result[`+ ${key}`] = obj2[key];
+    }
+  }
+  return result;
+};
 
 const genDiff = (filePath1, filePath2) => {
   const fullPath1 = getFullPath(filePath1);
@@ -13,5 +41,7 @@ const genDiff = (filePath1, filePath2) => {
   const data2 = getData(fullPath2);
   console.log(data1);
   console.log(data2);
+  const diffFile = differents(data1, data2);
+  console.log(diffFile);
 };
 export default genDiff;
