fb732ab5cce3344a546260259905e755a43c8f6e
Kseniya Freeflyer <kalyakina.ks.a@gmail.com>
2024-09-04 10:55:36 +0300

    add tests fixtures and file yml

--- __tests__/gendiff.tests.js
+++ __tests__/gendiff.tests.js
@@ -1,6 +1,7 @@
 import gendiff from "../index.js";
 import path from "path";
 import {fileURLToPath} from "url";
+import * as fs from "fs";
 
 test('test1 genDiff', () => {
 
@@ -15,22 +16,19 @@ test('test1 genDiff', () => {
 });
 
 // Получаю путь к текущему файлу
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url); // import.meta.url это свойство, доступное в модулях ECMAScript (ES-модулях), которое содержит URL текущего модуля.
+//fileURLToPath(url) принимает URL в качестве аргумента и возвращает путь файловой системы, соответствующий этому URL.
+const __dirname = path.dirname(__filename); //Метод path.dirname() возвращает имя каталога path, аналогично dirname команде Unix.
 
 test('test2 genDiff', () => {
     let path1file;
     path1file = path.join(__dirname, '..', '__fixtures__', 'file1.json');
     let path2file;
     path2file = path.join(__dirname, '..', '__fixtures__', 'file2.json');
+    let pathResultfile;
+    pathResultfile = path.join(__dirname, '..', '__fixtures__', 'file1_2_result.txt');
     const response = gendiff(path1file, path2file);
-
-    expect(response).toBe('{\n' +
-        '  - follow: false\n' +
-        '    host: hexlet.io\n' +
-        '  - proxy: 123.234.53.22\n' +
-        '  - timeout: 50\n' +
-        '  + timeout: 20\n' +
-        '  + verbose: true\n' +
-        '}');
+    // console.log('Response:', response);
+    // console.log('Expected:', fs.readFileSync(pathResultfile, 'utf8'));
+    expect(response.trim()).toBe(fs.readFileSync(pathResultfile, 'utf8').trim());
 });
