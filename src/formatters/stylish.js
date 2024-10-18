c60b5d5d03e46ed9470e6dc34e86a8cfb389eeea
ksu <kalyakina.ks.a@gmail.com>
2024-10-18 11:41:59 +0300

    improve stylish.js => no let in iter

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -19,84 +19,34 @@ const stylishIter = (key, value, iter = 1) => {
   const replacer = '    ';
   const beforeSymbol = replacer.repeat(iter);
   const shortBeforeSymbol = beforeSymbol.slice(0, -2);
-  let result = '';
-
-  const resultValue = `${value.beforeValue}`;
-  const separator = ' '; // resultValue.length > 0 ? ' ' : '';
-  let resultValue2;
-  let separator2;
-  let newIter;
   const { status } = value;
+  const valueIsNoObject = (value) => value === null || typeof value !== 'object';
 
-  switch (status) {
-    case 'equal':
-      if (value.beforeValue === null || typeof value.beforeValue !== 'object') {
-        result += `${beforeSymbol}${key}:${separator}${resultValue}\n`;
-      } else {
-        result += `${beforeSymbol}${key}: {\n`;
-        newIter = iter + 1;
-        result += printObj(value.beforeValue, newIter);
-        result += `${beforeSymbol}}\n`;
-      }
-      break;
-
-    case 'remove':
-      if (value.beforeValue === null || typeof value.beforeValue !== 'object') {
-        result += `${shortBeforeSymbol}- ${key}:${separator}${resultValue}\n`;
-      } else {
-        result += `${shortBeforeSymbol}- ${key}: {\n`;
-        newIter = iter + 1;
-        result += printObj(value.beforeValue, newIter);
-        result += `${beforeSymbol}}\n`;
-      }
-      break;
-
-    case 'add':
-      if (value.beforeValue === null || typeof value.beforeValue !== 'object') {
-        result += `${shortBeforeSymbol}+ ${key}:${separator}${resultValue}\n`;
-      } else {
-        result += `${shortBeforeSymbol}+ ${key}: {\n`;
-        newIter = iter + 1;
-        result += printObj(value.beforeValue, newIter);
-        result += `${beforeSymbol}}\n`;
-      }
-      break;
-
-    case 'different':
-      if (value.beforeValue === null || typeof value.beforeValue !== 'object') {
-        result += `${shortBeforeSymbol}- ${key}:${separator}${resultValue}\n`;
-      } else {
-        result += `${shortBeforeSymbol}- ${key}: {\n`;
-        newIter = iter + 1;
-        result += printObj(value.beforeValue, newIter);
-        result += `${beforeSymbol}}\n`;
-      }
-
-      resultValue2 = `${value.afterValue}`;
-      separator2 = ' '; // resultValue2.length > 0 ? ' ' : '';
-
-      if (value.afterValue === null || typeof value.afterValue !== 'object') {
-        result += `${shortBeforeSymbol}+ ${key}:${separator2}${resultValue2}\n`;
-      } else {
-        result += `${shortBeforeSymbol}+ ${key}: {\n`;
-        newIter = iter + 1;
-        result += printObj(value.afterValue, newIter);
-        result += `${beforeSymbol}}\n`;
-      }
-      break;
-
-    case 'difObject':
-      result += `${beforeSymbol}${key}: {\n`;
-      newIter = iter + 1;
-      result += gotoKeys(value.children, newIter);
-      result += `${beforeSymbol}}\n`;
-      break;
-
-    default:
-      console.log('error');
-      break;
+  if (status === 'equal') {
+    return valueIsNoObject(value.beforeValue)
+      ? `${beforeSymbol}${key}: ${value.beforeValue}\n`
+      : `${beforeSymbol}${key}: {\n${printObj(value.beforeValue, iter + 1)}${beforeSymbol}}\n`;
+  } if (status === 'remove') {
+    return valueIsNoObject(value.beforeValue)
+      ? `${shortBeforeSymbol}- ${key}: ${value.beforeValue}\n`
+      : `${shortBeforeSymbol}- ${key}: {\n${printObj(value.beforeValue, iter + 1)}${beforeSymbol}}\n`;
+  } if (status === 'add') {
+    return valueIsNoObject(value.beforeValue)
+      ? `${shortBeforeSymbol}+ ${key}: ${value.beforeValue}\n`
+      : `${shortBeforeSymbol}+ ${key}: {\n${printObj(value.beforeValue, iter + 1)}${beforeSymbol}}\n`;
+  } if (status === 'different') {
+    const getPrefix = (key, sign) => `${shortBeforeSymbol}${sign} ${key}: `;
+    const before = valueIsNoObject(value.beforeValue)
+      ? `${getPrefix(key, '-')}${value.beforeValue}\n`
+      : `${getPrefix(key, '-')}{\n${printObj(value.beforeValue, iter + 1)}${beforeSymbol}}\n`;
+    const after = valueIsNoObject(value.afterValue)
+      ? `${getPrefix(key, '+')}${value.afterValue}\n`
+      : `${getPrefix(key, '+')}{\n${printObj(value.afterValue, iter + 1)}${beforeSymbol}}\n`;
+    return `${before}${after}`;
+  } if (status === 'difObject') {
+    return `${beforeSymbol}${key}: {\n${gotoKeys(value.children, iter + 1)}${beforeSymbol}}\n`;
   }
-  return result;
+  return 'error';
 };
 
 const gotoKeys = (obj, iter = 1) => {
@@ -110,11 +60,6 @@ const gotoKeys = (obj, iter = 1) => {
   );
 };
 
-const stylish = (resultObj) => {
-  let result = '{\n';
-  result += gotoKeys(resultObj);
-  result += '}';
-  return result;
-};
+const stylish = (resultObj) => `{\n${gotoKeys(resultObj)}}`;
 
 export default stylish;
