ed03346bbf079ab5d03b0d8d10f698b9bff2d040
ksu <kalyakina.ks.a@gmail.com>
2024-10-21 14:55:18 +0300

    improve tests

--- __tests__/gendiff.tests.js
+++ __tests__/gendiff.tests.js
@@ -3,68 +3,47 @@ import { fileURLToPath } from 'url';
 import * as fs from 'fs';
 import gendiff from '../src/index.js';
 
-// Получаю путь к текущему файлу
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const getFixture = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFixtureFile = (filename) => fs.readFileSync(getFixture(filename), 'utf-8');
 
-const getFixtures = (files, result) => {
-  const filePaths = {
-    json: [
-      path.join(__dirname, '..', '__fixtures__', 'file1.json'),
-      path.join(__dirname, '..', '__fixtures__', 'file2.json'),
-    ],
-    yml: [
-      path.join(__dirname, '..', '__fixtures__', 'file1.yml'),
-      path.join(__dirname, '..', '__fixtures__', 'file2.yml'),
-    ],
-  };
-  const resultPaths = {
-    default: path.join(__dirname, '..', '__fixtures__', 'files_12_default_result.txt'),
-    plain: path.join(__dirname, '..', '__fixtures__', 'files_12_plain_result.txt'),
-  };
-  const [file1Path, file2Path] = filePaths[files];
-  const fileResultPath = resultPaths[result];
-
-  return [file1Path, file2Path, fileResultPath];
-};
-
-test('test1 genDiff .json default formatter', () => {
-  const [file1Path, file2Path, fileResultPath] = getFixtures('json', 'default');
-  const response = gendiff(file1Path, file2Path);
-  const fileContent = fs.readFileSync(fileResultPath, 'utf8');
-  const trimmedResponse = response.split('\n').map((line) => line.trimEnd()).join('\n').trim();
-  const trimmedFileContent = fileContent.split('\n').map((line) => line.trimEnd()).join('\n').trim();
-  expect(trimmedResponse).toBe(trimmedFileContent);
+test('test1 genDiff .json default=stylish formatter', () => {
+  const file1 = getFixture('file1.json');
+  const file2 = getFixture('file2.json');
+  const response = gendiff(file1, file2);
+  const fileContent = readFixtureFile('files_12_stylish_result.txt');
+  expect(response).toEqual(fileContent);
 });
 
-test('test2 genDiff .yml default formatter', () => {
-  const [file1Path, file2Path, fileResultPath] = getFixtures('yml', 'default');
-  const response = gendiff(file1Path, file2Path);
-  const fileContent = fs.readFileSync(fileResultPath, 'utf8');
-  const trimmedResponse = response.split('\n').map((line) => line.trimEnd()).join('\n').trim();
-  const trimmedFileContent = fileContent.split('\n').map((line) => line.trimEnd()).join('\n').trim();
-  expect(trimmedResponse).toBe(trimmedFileContent);
+test('test2 genDiff .yml default=stylish formatter', () => {
+  const file1 = getFixture('file1.yml');
+  const file2 = getFixture('file2.yml');
+  const response = gendiff(file1, file2);
+  const fileContent = readFixtureFile('files_12_stylish_result.txt');
+  expect(response).toBe(fileContent);
 });
 
 test('test3 genDiff .json PLAIN formatter', () => {
-  const [file1Path, file2Path, fileResultPath] = getFixtures('json', 'plain');
-  const response = gendiff(file1Path, file2Path, 'plain');
-  const fileContent = fs.readFileSync(fileResultPath, 'utf8');
-  const trimmedResponse = response.split('\n').map((line) => line.trimEnd()).join('\n').trim();
-  const trimmedFileContent = fileContent.split('\n').map((line) => line.trimEnd()).join('\n').trim();
-  expect(trimmedResponse).toBe(trimmedFileContent);
+  const file1 = getFixture('file1.yml');
+  const file2 = getFixture('file2.yml');
+  const response = gendiff(file1, file2, 'plain');
+  const fileContent = readFixtureFile('files_12_plain_result.txt');
+  expect(response).toBe(fileContent);
 });
 
 test('test4 genDiff JSON formatter', () => {
-  const [file1Path, file2Path] = getFixtures('json', 'default');
+  const file1 = getFixture('file1.yml');
+  const file2 = getFixture('file2.yml');
   const outputFilePath = path.join(__dirname, '..', 'output.json');
-  gendiff(file1Path, file2Path, 'json');
+  gendiff(file1, file2, 'json');
   const data = fs.readFileSync(outputFilePath, 'utf-8');
   expect(data).toBeTruthy();
 });
 
 test('test5 format error', () => {
-  const [file1Path, file2Path] = getFixtures('json', 'default');
-  const response = gendiff(file1Path, file2Path, 'qwe123');
+  const file1 = getFixture('file1.yml');
+  const file2 = getFixture('file2.yml');
+  const response = gendiff(file1, file2, 'qwe123');
   expect(response.trim()).toBe('error');
 });
